bdf36743429a387c310b57091cf997591778ff9c
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-04-02 10:26:49 +1000

    Adding Hello World!

---
+++ Problems/Hello_World.cpp
@@ -0,0 +1,13 @@
+/*
+  Kattis.com
+  Problem Hello World!.
+  Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: April 2nd, 2017
+ */
+
+#include <cstdio>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
