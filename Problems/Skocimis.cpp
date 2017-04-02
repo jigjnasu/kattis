c9ef26cb4baa1623e6fdf121f9d87132eaddaad7
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-04-03 02:28:03 +1000

    Adding solution to Skocimis

--- Problems/Skocimis.cpp
+++ Problems/Skocimis.cpp
@@ -4,3 +4,20 @@
   Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
   Date: April 3rd, 2017
  */
+
+#include <cstdio>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    scanf("%d %d %d", &a, &b, &c);
+
+    int distance = b - a - 1;
+    if (c - b - 1 > distance)
+        distance = c - b - 1;
+
+    printf("%d\n", distance);
+    
+    return 0;
+}
