15e6ad1be9863d28973ba12e93b14d7ead0807ce
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-04-03 22:43:17 +1000

    Adding solution to problem Solving for Carrots

---
+++ Problems/Solving_for_Carrots.cpp
@@ -0,0 +1,28 @@
+/*
+  Kattis.com
+  Problem Solving for Carrots.
+  Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: April 3rd, 2017
+ */
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int c = 0;
+    int p = 0;
+
+    std::cin >> c;
+    std::cin >> p;
+
+    std::cin.ignore();
+
+    while (c--) {
+        std::string line;
+        std::getline(std::cin, line);
+    }
+
+    std::cout << p << std::endl;
+    
+    return 0;
+}
