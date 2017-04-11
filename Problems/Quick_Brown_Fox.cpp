804b015b16b0cc0e820f576ac53c463226a489e7
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-04-11 22:25:16 +1000

    Quick Brown Fox Solution

--- Problems/Quick_Brown_Fox.cpp
+++ Problems/Quick_Brown_Fox.cpp
@@ -5,7 +5,55 @@ Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
 Date: 11/04/2017
 */
 
-int main() {
+#include <iostream>
+#include <string>
 
+// will have a, b, c, d ..... x, y, z.
+// Index will have 2^0 + 2^1 + ... + 2^25
+// This value is pre calculated one.
+const int panagram_digit = 67108863;
+
+int index(char c) {
+    int i = -1;
+    if (c >= 'a' && c <= 'z')
+        i = c - 'a';
+    else if (c >= 'A' && c <= 'Z')
+        i = c - 'A';
+
+    return i;
+}
+
+std::string missing(int total) {
+    std::string miss;
+    for (int i = 0; i < 26; ++i)
+        if ((total & 1 << i) == 0)
+            miss += 'a' + i;
+
+    return miss;
+}
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    std::cin.ignore();
+
+    while (n--) {
+        std::string line;
+        std::getline(std::cin, line);
+
+        int total = 0;
+        for (std::size_t i = 0; i < line.size(); ++i) {
+            const int pos = index(line[i]);
+
+            if (pos >= 0 && (total & 1 << pos) == 0)
+                total += 1 << pos;
+        }
+
+        if (panagram_digit == total)
+            std::cout << "pangram" << std::endl;
+        else
+            std::cout << "missing " << missing(total) << std::endl;
+    }
+    
     return 0;
 }
