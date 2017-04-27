7492a9a007d8163ad2f0703f65e9cd5aa6cb4000
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-04-28 02:33:40 +1000

    Adding solution for Printing Costs

--- Problems/Printing_Costs.cpp
+++ Problems/Printing_Costs.cpp
@@ -2,28 +2,39 @@
   Kattis.com
   Problem Printing Costs.
   Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
-  Date: 21/04/2017
- */
+  Date: 27/04/2017
+*/
 
-#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
 
-const std::string chars = " !#$%&'()*+   7        -   7        .   4        /  10        0  22        1  19
-2  22        3  23        4  21        5  27        6  26        7  16
-8  23        9  26        :   8        ;  11        <  10        =  14
->  10        ?  15        @  32        A  24        B  29        C  20
-D  26        E  26        F  20        G  25        H  25        I  18
-J  18        K  21        L  16        M  28        N  25        O  26
-P  23        Q  31        R  28        S  25        T  16        U  23
-V  19        W  26        X  18        Y  14        Z  22        [  18
-\  10        ]  18        ^   7        _   8        `   3        a  23
-b  25        c  17        d  25        e  23        f  18        g  30
-h  21        i  15        j  20        k  21        l  16        m  22
-n  18        o  20        p  25        q  25        r  13        s  21
-t  17        u  17        v  13        w  19        x  13        y  24
-z  19        {  18        |  12        }  18        ~   9';
+const std::vector<int> toner = {  0,   9,  6, 24, 29, 22,
+                                  24,  3, 12, 12, 17, 13,
+                                  7,  7,  4, 10, 22, 19,
+                                  22, 23, 21, 27, 26, 16,
+                                  23, 26,  8, 11, 10, 14,
+                                  10, 15, 32, 24, 29, 20,
+                                  26, 26, 20, 25, 25, 18,
+                                  18, 21, 16, 28, 25, 26,
+                                  23, 31, 28, 25, 16, 23,
+                                  19, 26, 18, 14, 22, 18,
+                                  10, 18,  7,  8,  3, 23,
+                                  25, 17, 25, 23, 18, 30,
+                                  21, 15, 20, 21, 16, 22,
+                                  18, 20, 25, 25, 13, 21,
+                                  17, 17, 13, 19, 13, 24,
+                                  19, 18, 12, 18, 9 };
 
 int main() {
-    printf("%d\n", ',');
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        int cost = 0;
+        for (std::size_t i = 0; i < line.size(); ++i)
+            cost += toner[line[i] - 32];
+
+        std::cout << cost << std::endl;
+    }
 
     return 0;
 }
