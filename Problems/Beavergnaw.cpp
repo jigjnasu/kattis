6bb28ba5cbcb1d60d85924bf8ce045ec49f57ff4
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-05-05 11:51:42 +1000

    adding solution for Beavergnaw

---
+++ Problems/Beavergnaw.cpp
@@ -0,0 +1,24 @@
+/*
+  Kattis.com
+  Problem Beavergnaw.
+  Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: 05/05/2017
+ */
+
+#include <cstdio>
+#include <cmath>
+
+int main() {
+    while (1) {
+        int D = 0;
+        int V = 0;
+        scanf("%d %d", &D, &V);
+
+        if (D == 0 && V == 0)
+            break;
+        
+        printf("%.9lf\n", std::pow(D * D * D - 6 * (V / M_PI), 1.0 / 3));
+    }
+        
+    return 0;
+}
