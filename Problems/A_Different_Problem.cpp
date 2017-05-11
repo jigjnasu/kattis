4652e84a23befd2040a4a90178fa202c1dff25aa
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-05-11 20:09:05 +1000

    Optimize

--- Problems/A_Different_Problem.cpp
+++ Problems/A_Different_Problem.cpp
@@ -10,19 +10,14 @@
 #include <cmath>
 
 int main() {
-    while (1) {
-        std::string line;
-        std::getline(std::cin, line);
-        
-        if (line.size() == 0)
-            break;
-        
+    std::string line;
+    while (std::getline(std::cin, line)) {
         const std::size_t pos = line.find(" ");
         double a = std::atof(line.substr(0, pos).c_str());
         double b = std::atof(line.substr(pos + 1, line.size()).c_str());
 
         printf("%.0lf\n", std::abs(a - b));
     }
-    
+
     return 0;
 }
