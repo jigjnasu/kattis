7ce841cf9b5eeb24b66fab14f78e28fa2c625eeb
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-08-10 23:52:19 +1000

    adding solution for Identifying Map Tiles

---
+++ Problems/maptiles2.cpp
@@ -0,0 +1,28 @@
+/*
+  Kattis.com
+  Problem Identifying Map Tiles.
+  Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: 10/08/2017
+ */
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s;
+    std::cin >> s;
+
+    int x = 0;
+    int y = 0;
+    int l = static_cast<int>(s.size());
+    for (std::size_t i = 0; i < l; ++i) {
+        if (s[i] == '1' || s[i] == '3')
+            x += (1 << l) >> (i + 1);
+        if (s[i] == '2' || s[i] == '3')
+            y += (1 << l) >> (i + 1);
+    }
+
+    std::cout << l << " " << x << " " << y << std::endl;
+
+    return 0;
+}
