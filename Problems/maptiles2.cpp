d33a804e601746a80336e8940e1fbb0da9d6a38d
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-08-11 00:09:04 +1000

    modify solution for Identifying Map Tiles

--- Problems/maptiles2.cpp
+++ Problems/maptiles2.cpp
@@ -17,9 +17,9 @@ int main() {
     int l = static_cast<int>(s.size());
     for (std::size_t i = 0; i < l; ++i) {
         if (s[i] == '1' || s[i] == '3')
-            x += (1 << l) >> (i + 1);
+            x += 1 << (l - i - 1);
         if (s[i] == '2' || s[i] == '3')
-            y += (1 << l) >> (i + 1);
+            y += 1 << (l - i - 1);
     }
 
     std::cout << l << " " << x << " " << y << std::endl;
