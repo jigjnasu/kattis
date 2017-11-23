98cc82e7629ff241853f0d1f52e1a3f42ae5e16a
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-11-24 00:05:33 +1100

    Solution for Sjecista

---
+++ Problems/sjecista.cpp
@@ -0,0 +1,22 @@
+/*
+  Kattis.com
+  Problem Help a PhD candidate out!.
+  Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: 24/11/2017
+ */
+
+#include <cstdio>
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+
+    int total = 0;
+    int j = 0;
+    for (int i = 1, j = n - 3; j > 0; ++i, --j)
+        total += i * j;
+
+    printf("%d\n", (n * total) / 4);
+
+    return 0;
+}
